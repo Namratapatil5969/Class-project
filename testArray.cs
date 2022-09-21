50a0cbfd12c8181b3de14ccecca5696b03f123c0
namratapatil656@gmail.com <namratapatil656@gmail.com>
2022-09-21 15:58:46 +0530

    Jagged Array

--- testArray.cs
+++ testArray.cs
@@ -83,26 +83,26 @@ namespace Class_program
         //    }
         //}
 
-        static void Main(string[] args)
-        {
-            // 3 - row
-            // 4- col
-            int[,] arr = new int[3, 4]
-            {
-               { 1,2,3,4},
-               {5,6,7,8 },
-               {9,10,11,12 }
-            };
+        //static void Main(string[] args)
+        //{
+        //    // 3 - row
+        //    // 4- col
+        //    int[,] arr = new int[3, 4]
+        //    {
+        //       { 1,2,3,4},
+        //       {5,6,7,8 },
+        //       {9,10,11,12 }
+        //    };
 
-            for (int i = 0; i < arr.GetLength(0); i++)  // GetLength(0) -> specifies row size
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)  // GetLength(1)-> specifies col size
-                {
-                    Console.Write($"arr[{i},{j}]= {arr[i, j]}  ");
-                }
-                Console.WriteLine();
-            }
-        }
+        //    for (int i = 0; i < arr.GetLength(0); i++)  // GetLength(0) -> specifies row size
+        //    {
+        //        for (int j = 0; j < arr.GetLength(1); j++)  // GetLength(1)-> specifies col size
+        //        {
+        //            Console.Write($"arr[{i},{j}]= {arr[i, j]}  ");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
 
 
 
